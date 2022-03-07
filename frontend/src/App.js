9e3e6f283fa281ddd098ae5af1d1fdf3e62c8f5b
CeduDev <cedric.neergaard@gmail.com>
2022-03-07 17:00:59 +0200

    hello_world

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Alert, Button } from "react-bootstrap";
+//import ReactNotifications from 'react-browser-notifications';
 
-function App() {
+const App = () => {
+
+  const setButtonClicked = () => {
+    if(this.n.supported()) this.n.show();
+  }
+
+  /*const handleClick = (event) => {
+    event.preventDefault();
+    console.log('boi')
+  }*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hello world!</h1>
+      <Alert>hello</Alert>
+      <Button onClick={setButtonClicked}>yay</Button>
+{/*      <ReactNotifications
+          onRef={ref => (this.n = ref)} // Required
+          title="Hey There!" // Required
+          body="This is the body"
+          tag="abcdef"
+          timeout="2000"
+          onClick={event => handleClick(event)}
+        />
+  */}    </div>
   );
 }
 
 export default App;
+ 
