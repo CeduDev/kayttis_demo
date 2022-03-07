2f5faed371b7ca27e1248142ebc9a3721d037453
CeduDev <cedric.neergaard@gmail.com>
2022-03-07 17:33:51 +0200

    notification component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,39 @@
 import React from "react";
 import { Alert, Button } from "react-bootstrap";
-//import ReactNotifications from 'react-browser-notifications';
+import Notification from "react-web-notification";
 
 const App = () => {
-
   const setButtonClicked = () => {
-    if(this.n.supported()) this.n.show();
+    console.log('b')
   }
 
-  /*const handleClick = (event) => {
-    event.preventDefault();
-    console.log('boi')
-  }*/
+  const onShow = () => {
+    console.log('on show')
+  }
+  const onClose = () => {
+    console.log('on close')
+  }
+  const onClick = () => {
+    console.log('on click')
+  }
 
   return (
     <div>
       <h1>Hello world!</h1>
       <Alert>hello</Alert>
       <Button onClick={setButtonClicked}>yay</Button>
-{/*      <ReactNotifications
-          onRef={ref => (this.n = ref)} // Required
-          title="Hey There!" // Required
-          body="This is the body"
-          tag="abcdef"
-          timeout="2000"
-          onClick={event => handleClick(event)}
+      <Notification
+          ignore={false}
+          onShow={onShow}
+          onClick={onClick}
+          onClose={onClose}
+          onError={() => {
+            console.log('error')
+          }}
+          timeout={5000}
+          title={'very nice title'}
         />
-  */}    </div>
+    </div>
   );
 }
 
