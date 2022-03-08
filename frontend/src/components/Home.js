59853a8ac7c248e5e555b366116d3e0eb3e36897
CeduDev <cedric.neergaard@gmail.com>
2022-03-08 19:18:06 +0200

    home, emoji, myday

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
-import { Container, Row, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Button, Col } from 'react-bootstrap';
 import Notification from 'react-web-notification';
+import Emoji from './Emoji';
 
 const Home = () => {
   const [ignore, setIgnore] = useState(false);
   const [title, setTitle] = useState('');
   const [options, setOptions] = useState({});
   const [buttonClicked, setButtonClicked] = useState(0);
+  const [todaysDay, setTodaysDay] = useState('Monday');
+
+  useEffect(() => {
+    const days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
+    setTodaysDay(days[new Date().getDay()]);
+  }, []);
 
   const handleButtonClick = () => {
     if (ignore) {
@@ -35,33 +51,52 @@ const Home = () => {
   };
 
   const handlePermissionGranted = () => {
-    console.log('Permission Granted');
     setIgnore(false);
   };
   const handlePermissionDenied = () => {
-    console.log('Permission Denied');
     setIgnore(true);
   };
   const handleNotSupported = () => {
-    console.log('Web Notification not Supported');
     setIgnore(true);
   };
 
   return (
     <Container>
       <Row>
-        <h2>Welcome!</h2>
+        <h2>
+          Welcome! <Emoji symbol="👋" label="hand_wave" />
+        </h2>
       </Row>
       <Row>
-        <Button onClick={handleButtonClick}>Test Notifications</Button>
-        <Notification
-          ignore={ignore}
-          title={title}
-          options={options}
-          notSupported={handleNotSupported}
-          onPermissionGranted={handlePermissionGranted}
-          onPermissionDenied={handlePermissionDenied}
-        />
+        <p>
+          Continuous logins: <strong>10 days</strong>, which is longer than 60%
+          of the average user <Emoji symbol="💪" label="strong" />
+        </p>
+      </Row>
+      <Row>
+        <p>
+          Today it's a sunny {todaysDay}, maybe try out this cool{' '}
+          <a
+            href="https://www.themuse.com/advice/work-from-home-productivity-schedule"
+            target="_blank"
+            rel="noreferrer"
+          >
+            new routine?
+          </a>
+        </p>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={handleButtonClick}>Test Notifications</Button>
+          <Notification
+            ignore={ignore}
+            title={title}
+            options={options}
+            notSupported={handleNotSupported}
+            onPermissionGranted={handlePermissionGranted}
+            onPermissionDenied={handlePermissionDenied}
+          />
+        </Col>
       </Row>
     </Container>
   );
