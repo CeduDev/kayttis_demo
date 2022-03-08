4706e51a30e5f5ad2f713037aadf91face7f320f
CeduDev <cedric.neergaard@gmail.com>
2022-03-08 16:58:48 +0200

    prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,61 @@
-import React, { useState } from "react";
-import { Button, Tabs, Tab, Row, Container } from "react-bootstrap";
-import Notification from "react-web-notification";
+import React, { useState } from 'react';
+import { Button, Tabs, Tab, Row, Container } from 'react-bootstrap';
+import Notification from 'react-web-notification';
 
 const App = () => {
-  const [ignore, setIgnore] = useState(false)
-  const [title, setTitle] = useState('')
-  const [options, setOptions] = useState({})
+  const [ignore, setIgnore] = useState(false);
+  const [title, setTitle] = useState('');
+  const [options, setOptions] = useState({});
 
   const handleButtonClick = () => {
     if (ignore) {
       return;
     }
-    setTitle('Test')
+    setTitle('Test');
     setOptions({
       tag: Date.now(),
       body: `Test: ${Date.now()}`,
       lang: 'eng',
       dir: 'ltr',
-    })
-  }
+    });
+  };
 
   const handlePermissionGranted = () => {
     console.log('Permission Granted');
-    setIgnore(false)
-  }
+    setIgnore(false);
+  };
   const handlePermissionDenied = () => {
     console.log('Permission Denied');
-    setIgnore(true)
-  }
+    setIgnore(true);
+  };
   const handleNotSupported = () => {
     console.log('Web Notification not Supported');
-    setIgnore(true)
-  }
+    setIgnore(true);
+  };
 
-
-    return (
-      <Container>
-        <Row>
-          <h1>Habit House</h1>
-        </Row>
-        <Row>
-          <Tabs defaultActiveKey="home" id="kakka" className="mb-3">
-            <Tab eventKey="home" title="Home">
-                <div>home</div>
-        <Button onClick={handleButtonClick}>test</Button>
-              </Tab>  
-            <Tab eventKey="myDay" title="My Day">
-                <div>my day</div>
-              </Tab>  
-            <Tab eventKey="routines" title="Routines">
-                <div>routines</div>
-              </Tab>  
-            <Tab eventKey="stats" title="Stats">
-                <div>stats</div>
-              </Tab>  
-          </Tabs>
-        </Row>
-        <Row>
-
+  return (
+    <Container>
+      <Row>
+        <h1>Habit House</h1>
+      </Row>
+      <Row>
+        <Tabs defaultActiveKey="home" id="kakka" className="mb-3">
+          <Tab eventKey="home" title="Home">
+            <div>home</div>
+            <Button onClick={handleButtonClick}>test</Button>
+          </Tab>
+          <Tab eventKey="myDay" title="My Day">
+            <div>my day</div>
+          </Tab>
+          <Tab eventKey="routines" title="Routines">
+            <div>routines</div>
+          </Tab>
+          <Tab eventKey="stats" title="Stats">
+            <div>stats</div>
+          </Tab>
+        </Tabs>
+      </Row>
+      <Row>
         <Notification
           ignore={ignore}
           title={title}
@@ -66,11 +64,9 @@ const App = () => {
           onPermissionGranted={handlePermissionGranted}
           onPermissionDenied={handlePermissionDenied}
         />
-          </Row>
-      </Container>
-
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
- 
