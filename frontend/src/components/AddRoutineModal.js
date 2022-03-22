a9b785f6eff338f81f241b1d3b99ee23e59ddfae
aronekman <aron.ekman@aalto.fi>
2022-03-22 16:51:56 +0200

    default breaks

--- frontend/src/components/AddRoutineModal.js
+++ frontend/src/components/AddRoutineModal.js
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from 'react';
-import { Button, Modal, Row, Col, Alert } from 'react-bootstrap';
+import { Button, Modal, Row, Col, Alert, Dropdown } from 'react-bootstrap';
 import { useMainStore } from '../stores/MainStore';
 
 const AddRoutineModal = observer(({ routine, setRoutine }) => {
@@ -17,15 +17,14 @@ const AddRoutineModal = observer(({ routine, setRoutine }) => {
 
   const [breaks, setBreaks] = useState([]);
 
-  const [breakAlertMessage, setBreakAlertMessage] = useState('')
-  const [alertMessage, setAlertMessage] = useState('')
-
+  const [breakAlertMessage, setBreakAlertMessage] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
 
   const store = useMainStore();
 
   const handleSubmit = () => {
     if (title !== '' && startTime !== '' && endTime !== '' && !showAlert) {
-      setAlertMessage('')
+      setAlertMessage('');
       const start = startTime.split(':').map((x) => Number(x));
       const end = endTime.split(':').map((x) => Number(x));
       const startDate = new Date();
@@ -44,19 +43,18 @@ const AddRoutineModal = observer(({ routine, setRoutine }) => {
       setShow(false);
     } else {
       if (title === '') {
-        setAlertMessage('Routine name missing')
+        setAlertMessage('Routine name missing');
       } else if (startTime === '') {
         setAlertMessage('Start time missing');
       } else {
-        setAlertMessage('End time missing')
+        setAlertMessage('End time missing');
       }
     }
   };
 
   const handleAddBreak = () => {
-    console.log(breakType);
     if (breakType !== '' && breakStartTime !== '' && breakEndTime !== '') {
-      setBreakAlertMessage('')
+      setBreakAlertMessage('');
       const start = breakStartTime.split(':').map((x) => Number(x));
       const end = breakEndTime.split(':').map((x) => Number(x));
       const startDate = new Date();
@@ -78,17 +76,13 @@ const AddRoutineModal = observer(({ routine, setRoutine }) => {
       setBreakEndTime('');
     } else {
       if (breakType === '') {
-        setBreakAlertMessage('Type missing')
-
+        setBreakAlertMessage('Type missing');
       } else if (breakStartTime === '') {
         setBreakAlertMessage('Start time missing');
-
       } else {
-        setBreakAlertMessage('End time missing')
-        
+        setBreakAlertMessage('End time missing');
       }
     }
-    
   };
 
   const pad = (toPad) => {
@@ -105,6 +99,24 @@ const AddRoutineModal = observer(({ routine, setRoutine }) => {
       setShowAlert(false);
     }
   }, [store.routines, title]);
+
+  const defaultBreaks = [
+    {
+      description: 'coffee break',
+      start: '09:30',
+      end: '09:45',
+    },
+    {
+      description: 'lunch',
+      start: '12:00',
+      end: '12:30',
+    },
+    {
+      description: 'walk',
+      start: '14:00',
+      end: '14:30',
+    },
+  ];
 
   return (
     <>
@@ -165,12 +177,32 @@ const AddRoutineModal = observer(({ routine, setRoutine }) => {
               onChange={(event) => setBreakEndTime(event.target.value)}
             ></input>
             <br />
-            <Row style={{ width: '100%' }} className="addBreakButtonRow">
+            <Row className="addBreakButtonRow">
               <Button className="addBreakButton" onClick={handleAddBreak}>
                 Add Break
               </Button>
+              <Dropdown>
+                <Dropdown.Toggle>Default breaks</Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {defaultBreaks.map((b) => {
+                    return (
+                      <Dropdown.Item
+                        onClick={() => {
+                          setBreakType(b.description);
+                          setBreakStartTime(b.start);
+                          setBreakEndTime(b.end);
+                        }}
+                      >
+                        {b.description}: {b.start}-{b.end}
+                      </Dropdown.Item>
+                    );
+                  })}
+                </Dropdown.Menu>
+              </Dropdown>
             </Row>
-            {breakAlertMessage !== '' && <Alert variant='danger'>{breakAlertMessage}</Alert>}
+            {breakAlertMessage !== '' && (
+              <Alert variant="danger">{breakAlertMessage}</Alert>
+            )}
           </div>
           {breaks.map((b, idx) => (
             <Col className="breakCol" key={idx}>
@@ -189,13 +221,16 @@ const AddRoutineModal = observer(({ routine, setRoutine }) => {
             </Col>
           ))}
         </Modal.Body>
-        <Modal.Footer style={{padding: '0rem'}}className='me-2'>
-          
+        <Modal.Footer style={{ padding: '0rem' }} className="me-2">
           <Button variant="success" onClick={handleSubmit}>
             Add routine
           </Button>
         </Modal.Footer>
-        {alertMessage !== '' && <Alert className='ms-2 me-2 mb-1'variant='danger'>{alertMessage}</Alert>}
+        {alertMessage !== '' && (
+          <Alert className="ms-2 me-2 mb-1" variant="danger">
+            {alertMessage}
+          </Alert>
+        )}
       </Modal>
     </>
   );
